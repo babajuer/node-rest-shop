9c56b1e6d64a2e4f64b88224f729f3433811855b
Babajuer <Babajuer@fangs-macbook-pro.home>
2019-03-15 15:11:43 +0100

    feat(login): #12 user login and token

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const monggose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
@@ -53,6 +55,49 @@ router.post('/signup', (req, res, next) => {
     });    
 })
 
+
+router.post('/login', (req, res, next) => {
+    User.find({
+            email: req.body.email
+        })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Auth failed000'
+                })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed 111'
+                    })
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, 
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn: "1h"
+                    });
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    })
+                }
+                return res.status(401).json({
+                    message: 'Auth failed 222'
+                })
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+})
 
 router.delete('/:userId', (req, res, next) => {
     console.log( ` 111111:`, req.params);
